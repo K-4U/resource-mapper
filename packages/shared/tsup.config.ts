7df71f0d649ddf487cd4ea34aae9bdd96c58713a
Koen Beckers <koen@k-4u.nl>
2026-03-07 12:45:35 +0100

    Some release fixes. Needs more work.

--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -1,17 +1,15 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-    entry: ['src/**/*.ts'],
+    entry: ['src/**/*.ts', '!src/**/*.test.ts'],
 
     format: ['esm'],
     target: 'node22',
     platform: 'node',
-    bundle: false, // Keep this false to avoid the "events" error
+    bundle: false,
     clean: true,
     shims: true,
     sourcemap: true,
-    noExternal: [/.*/],
 
-    // This ensures the folder structure in 'src' is mirrored in 'dist'
     outDir: 'dist',
 });
